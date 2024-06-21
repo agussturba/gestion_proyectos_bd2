aca3574444b8d80ccd7519504be7ba5f9307ca3d
Agustin Manuel Sturba <agustin.sturba@mercadolibre.com>
2024-06-21 14:30:25 -0300

    Fixing models and services

--- src/main/java/com/grupo/bd2/repository/TaskProgressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grupo.bd2.repository;
-import com.grupo.bd2.model.TaskProgress;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskProgressRepository extends JpaRepository<TaskProgress, Long> {
-}
